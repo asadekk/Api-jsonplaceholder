6a6da577b6a2e4a7aae73acb4da24136819ad38b
asadekk <asadesd21@gmail.com>
2026-05-10 23:49:39 +0500

    yuklash

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -5,3 +5,6 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com"
 })
+export const apiUsers = axios.create({
+  baseURL:"https://jsonplaceholder.typicode.com"
+})
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,7 +1,12 @@
 import { api } from "./api";
+import { apiUsers } from "./api";
 
 
 export async function getPost() {
     let res = await api.get ("/posts");
     return res;
+}
+export async function getUsers() {
+    let res = apiUsers.get ("/users");
+    return res
 }
